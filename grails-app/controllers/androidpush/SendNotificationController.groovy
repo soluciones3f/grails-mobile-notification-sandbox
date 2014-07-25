f68830ce9f480d84341c182eaa02bab292268893
pabloaguayo <pablo.aguayo@soluciones3f.com.ar>
2014-07-25 12:37:32 -0300

    arreglo como se envia el mensaje para que tome la data

--- grails-app/controllers/androidpush/SendNotificationController.groovy
+++ grails-app/controllers/androidpush/SendNotificationController.groovy
@@ -13,15 +13,16 @@ class SendNotificationController {
             withHttp(uri: "https://android.googleapis.com/gcm/") {
                 def bodyContent = [
                        registration_id: device,
-                       data: "{'message':'Hola','msgcnt':'1'}",
-                       message:'Hola',
-                       msgcnt: '1'
+                       
+                        "data.message":"Hola",
+                        "data.msgcnt":"1",
+                        "data.title":"prueba"
+                        
                     ]
                 def json = post(path: 'send', 
                     body: bodyContent, 
                     headers:['Authorization':'key=AIzaSyC-jnkrWsZq8BL32KMjiOJBHNxRjNTpHk8',
                         'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'])
-                println json
             }
         }
         render ""
